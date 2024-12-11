2cdd77136456ff809811312cb6d445390fe8cf4e
gaelg311 <gael@pdx.edu>
2024-12-11 05:11:53 -0800

    Form Complete, Intro Reformatted, Class Descriptions added

---
+++ contact-form.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("contact-form-button")
+  .addEventListener("click", function () {
+    //create modal background
+    const modal = document.createElement("div");
+  });
